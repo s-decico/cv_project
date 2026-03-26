07b617949d86b202e2fffef15041a22e7d5bc9da
Syamantak Sarkar <49286743+s-decico@users.noreply.github.com>
2026-03-26 17:59:22 +0530

    Google OAuth implementation

--- src/index.js
+++ src/index.js
@@ -9,20 +9,23 @@ import Registration from "./Pages/LoginForms/Registration";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CVInputBox from "./Pages/CVInputBox";
 import CVBox from "./Pages/CVBox";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <ThemeProvider theme={MuiThemeContext}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cvinput" element={<CVInputBox />} />
-          <Route path="/cv" element={<CVBox />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Registration />} />
-        </Routes>
-      </ThemeProvider>
-    </Router>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <Router>
+        <ThemeProvider theme={MuiThemeContext}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cvinput" element={<CVInputBox />} />
+            <Route path="/cv" element={<CVBox />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Registration />} />
+          </Routes>
+        </ThemeProvider>
+      </Router>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
